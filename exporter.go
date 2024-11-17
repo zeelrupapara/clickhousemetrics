dc9ca377c0f71783670ce7e72bcf7bf4169361ec
zeelrupapara <zeelrupapara@gmail.com>
2024-11-17 23:49:39 +0530

    fix: insert query

--- exporter.go
+++ exporter.go
@@ -163,7 +163,7 @@ func (s *Metrics) Insert(ctx context.Context, db *sql.DB) error {
 	if s.count == 0 {
 		return nil
 	}
-	const insertQuery = "INSERT INTO metrics (MetricName, STtartTime, MetricUnit, StartTimeUnix, TimeUnix, Value) VALUES(?, ?, ?, ?, ?, ?)"
+	const insertQuery = "INSERT INTO metrics (MetricName, StartTime, MetricUnit, StartTimeUnix, TimeUnix, Value) VALUES(?, ?, ?, ?, ?, ?)"
 	start := time.Now()
 	err := doWithTx(ctx, db, func(tx *sql.Tx) error {
 		statement, err := tx.PrepareContext(ctx, insertQuery)
